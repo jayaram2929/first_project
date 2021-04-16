fe97d6ebacc898349d1cd6e1109336935fb510ef
jayaram2929 <76050370+jayaram2929@users.noreply.github.com>
2021-04-16 18:19:11 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,3 +1,5 @@
-public static void main(){
-  System.out.println("Hello World!")
+class HelloWorld {
+    public static void main( String []args ) {
+        System.out.println( "Hello World!" );
+    }
 }
